c40d16e69973e6026354a15318d732f6bd3b941c
Shubham Rawat <shubhamrawat@Shubhams-MacBook-Air.local>
2026-03-28 14:25:17 +0530

    data provider feature with tables and search

---
+++ src/constants/index.tsx
@@ -0,0 +1,3 @@
+export const DEPARTMENTS = ['CS','Maths','Physics','Chemistry','Biology','History','Geography','English','Economics','Computer Science']
+
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map(dept => ({label: dept, value: dept.toLowerCase()}))
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type Subjects = {
+    id: number;
+    name: string;
+    Code: string;
+    description: string;
+    department: string;
+    createdAt: Date;
+}
